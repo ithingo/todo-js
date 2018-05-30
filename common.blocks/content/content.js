1461109f9564085e561523e653c5f0b3db59f4b3
Vladimir Borodaev <v.borodaev@dunice.net>
2018-05-30 14:18:30 +0300

    Fixed adding new task item on button click

--- common.blocks/content/content.js
+++ common.blocks/content/content.js
@@ -1,39 +1,41 @@
-// import {addKeyupEvenListenerForInput, getFormInputValue} from "./form/form";
-
-const addButton = document.getElementById('add_button');
 const inputField = document.getElementById('form_input');
-const tasksList = document.getElementsByClassName('tasks_list')[0];
+const addButton = $('#add_button');
 
 const elementSelector = "tasks__item";
 
-const enterKeyCode = 13;
-const event = 'keyup';
-
-// const itemTemplate = '<li class="tasks_item"></li>';
-
-function addKeyupEvenListenerForInput(keyCode, eventName) {
-    inputField.addEventListener(eventName, (e) => {
-        e.preventDefault();
-        if (keyCode === enterKeyCode) {
-            addButton.click();
-        }
-    })
-}
+// const enterKeyCode = 13;
+// const event = 'keyup';
+//
+// function addKeyupEvenListenerForInput(keyCode, eventName) {
+//     inputField.addEventListener(eventName, (e) => {
+//         e.preventDefault();
+//         if (keyCode === enterKeyCode) {
+//             addButton.click();
+//         }
+//     })
+// }
 
 function getFormInputValue() {
     return inputField.value;
 }
 
-function addElement() {
-    let newElement = document.createElement('li');
-    newElement.setAttribute('class', elementSelector);
-    newElement.innerHTML = getFormInputValue();
-
-    tasksList.appendChild(newElement);
-}
+// function addElement() {
+    // let newElement = document.createElement('li');
+    // newElement.setAttribute('class', elementSelector);
+    // newElement.innerHTML = getFormInputValue();
+    // //
+    // // tasksList.appendChild(newElement);
+    //
+    // // $('.tasks__list')
+    // document.body.appendChild(newElement);
+// }
 
 $(document).ready(() => {
-    addButton.onclick = addElement()();
 
-    addKeyupEvenListenerForInput(enterKeyCode, event); //?
-})
+    addButton.click(() => {
+        let newElement = document.createElement('li');
+        newElement.setAttribute('class', elementSelector);
+        newElement.innerHTML = getFormInputValue();
+        document.body.appendChild(newElement);
+    });
+});
