65291cf6e4284b37666f0b9238ff53ec8413e5c2
Vladimir Borodaev <v.borodaev@dunice.net>
2018-05-30 14:51:30 +0300

    Fixed functionality for adding new item on Enter key press && A bit refactored

--- common.blocks/content/content.js
+++ common.blocks/content/content.js
@@ -1,19 +1,22 @@
 const inputField = document.getElementById('form_input');
 const addButton = $('#add_button');
+const itemsList = $('.tasks__list');
 
 const elementSelector = "tasks__item";
 
-// const enterKeyCode = 13;
-// const event = 'keyup';
-//
-// function addKeyupEvenListenerForInput(keyCode, eventName) {
-//     inputField.addEventListener(eventName, (e) => {
-//         e.preventDefault();
-//         if (keyCode === enterKeyCode) {
-//             addButton.click();
-//         }
-//     })
-// }
+const enterKey = 'Enter';
+const event = 'keyup';
+
+function addKeyupEvenListenerForInput(key, eventName) {
+    inputField.addEventListener(eventName, (e) => {
+        e.preventDefault();
+
+        //keCode is deprecated, use instead the value for entered key
+        if (e.key.toString() === key) {
+            addButton.click();
+        }
+    })
+}
 
 function getFormInputValue() {
     return inputField.value;
@@ -26,11 +29,13 @@ function addElement() {
 
     if (inputedValue !== '') {
         newElement.textContent = inputedValue;
-        document.body.appendChild(newElement);
+        // Add new item before existing in the list
+        itemsList.prepend(newElement);
     }
 }
 
 $(document).ready(() => {
+    addKeyupEvenListenerForInput(enterKey, event);
 
     addButton.click(() => {
         addElement();
