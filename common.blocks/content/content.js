e8077739dfee11dc46b9e2b587832cff021824ce
Vladimir Borodaev <v.borodaev@dunice.net>
2018-05-31 11:59:51 +0300

    Added object creation for every item to store in array and re-render withdrawing items by click

--- common.blocks/content/content.js
+++ common.blocks/content/content.js
@@ -1,11 +1,19 @@
 let inputField = document.getElementById('form_input');
+const itemsList = document.getElementById('task_list');
 const addButton = $('#add_button');
-const itemsList = $('.tasks__list');
+
 
 const elementSelector = "tasks__item";
 
 const enterKey = 'Enter';
 const event = 'keyup';
+
+const defaultStatusForTask = 'undone';
+const doneStatusForTasks = 'done';
+
+const tagTypeForItems = 'li';
+
+const itemListArray = Array();
 
 function addKeyupEvenListenerForInput(key, eventName) {
     inputField.addEventListener(eventName, (e) => {
@@ -18,31 +26,55 @@ function addKeyupEvenListenerForInput(key, eventName) {
     })
 }
 
-function getFormInputValue() {
+function getFormInputValue(inputField) {
     return inputField.value;
 }
 
-function clearInputField() {
+function clearInputField(inputField) {
     inputField.value = "";
 }
 
-function addElement() {
-    let newElement = document.createElement('li');
-    newElement.className = elementSelector;
-    let inputedValue = getFormInputValue();
-
-    if (inputedValue !== '') {
-        newElement.textContent = inputedValue;
-        // Add new item before existing in the list
-        itemsList.prepend(newElement);
+function createObjectFromNewValue(inputedValue) {
+    return {
+        inputedContent: inputedValue,
+        statusOfProgress: defaultStatusForTask,
     }
+}
+
+function addElement(array, inputField) {
+    let inputedValue = getFormInputValue(inputField);
+    if (inputedValue !== '') {
+        let newElement = createObjectFromNewValue(inputedValue);
+        array.push(newElement);
+    }
+}
+
+function createHtmlElementFromArrayElement(arrayElementInputedContent, tag, selector) {
+    return `<${tag} class="${selector}">${arrayElementInputedContent}`+'<'+"/"+`${tag}>`;
+}
+
+function createItemsTagsGroupFromArray(array, tag, selector) {
+    let itemsTagged = "";
+    for (let i = 0; i < array.length; i++) {
+        itemsTagged += createHtmlElementFromArrayElement(array[i].inputedContent, tag, selector);;
+    }
+    return itemsTagged;
+}
+
+function withdrawElements(itemList, itemListArray) {
+    const elementTaggedList = createItemsTagsGroupFromArray(itemListArray, tagTypeForItems, elementSelector);
+    alert(elementTaggedList);
+    itemList.innerHTML = elementTaggedList;
 }
 
 $(document).ready(() => {
     addKeyupEvenListenerForInput(enterKey, event);
 
     addButton.click(() => {
-        addElement();
-        clearInputField();
+        addElement(itemListArray, inputField);
+
+        clearInputField(inputField);
+
+        withdrawElements(itemsList, itemListArray);
     });
 });
