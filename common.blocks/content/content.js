48e4f418f6e781583ea276411ccad7c97e376a5f
Vladimir Borodaev <v.borodaev@dunice.net>
2018-05-31 15:14:50 +0300

    Added checkbox. Finaly added checking for checkbox

--- common.blocks/content/content.js
+++ common.blocks/content/content.js
@@ -2,11 +2,13 @@ let inputField = document.getElementById('form_input');
 const itemsList = document.getElementById('task_list');
 const addButton = $('#add_button');
 
-
 const elementSelector = "tasks__item";
+// const defaultStatusClassName = 'tasks__item_default';
+// const doneStatusClassName = 'tasks_item_done';
+const checkboxClassName = 'item_checkbox';
 
 const enterKey = 'Enter';
-const event = 'keyup';
+const keyEvent = 'keyup';
 
 const defaultStatusForTask = 'undone';
 const doneStatusForTasks = 'done';
@@ -15,8 +17,8 @@ const tagTypeForItems = 'li';
 
 const itemListArray = Array();
 
-function addKeyupEvenListenerForInput(key, eventName) {
-    inputField.addEventListener(eventName, (e) => {
+function addKeyupEvenListenerForInput(key, eventNameForKey) {
+    inputField.addEventListener(eventNameForKey, (e) => {
         e.preventDefault();
 
         //keCode is deprecated, use instead the value for entered key
@@ -44,13 +46,15 @@ function createObjectFromNewValue(inputedValue) {
 function addElement(array, inputField) {
     let inputedValue = getFormInputValue(inputField);
     if (inputedValue !== '') {
+        //add check for same content
         let newElement = createObjectFromNewValue(inputedValue);
         array.push(newElement);
     }
 }
 
 function createHtmlElementFromArrayElement(arrayElementInputedContent, tag, selector) {
-    return `<${tag} class="${selector}">${arrayElementInputedContent}`+'<'+"/"+`${tag}>`;
+    let partOfStringWithCheckbox = `<input type="checkbox" class="${checkboxClassName}">`;
+    return `<${tag} class="${selector}">`+ partOfStringWithCheckbox + `${arrayElementInputedContent}` + '<' + "/" + `${tag}>`;
 }
 
 function createItemsTagsGroupFromArray(array, tag, selector) {
@@ -63,12 +67,38 @@ function createItemsTagsGroupFromArray(array, tag, selector) {
 
 function withdrawElements(itemList, itemListArray) {
     const elementTaggedList = createItemsTagsGroupFromArray(itemListArray, tagTypeForItems, elementSelector);
-    alert(elementTaggedList);
     itemList.innerHTML = elementTaggedList;
+
+}
+// function findItemInArrayWithSameContent(htmlItemContent, itemsListArray) {
+//     // let foundedElement = itemsListArray.indexOf(htmlItemContent);
+//     let foundedElement = _.findWhere(itemsListArray, {inputedContent: htmlItemContent});
+//     alert(foundedElement.inputedContent);
+// }
+//
+// function isTaskDone(arrayElement) {
+//     return arrayElement.statusOfProgress === doneStatusForTasks;
+// }
+//
+// function changeItemStateIfSelected(itemList) {
+//
+// }
+
+function listenToItemsForClicking(checkboxSelector) {
+    const checkboxSelectorForJquery = '.' + checkboxSelector;
+    $(checkboxSelectorForJquery).change(function() {
+        if (this.checked) {
+            let chosenItemNode = this.parentNode;
+            // findItemInArrayWithSameContent(chosenItemNode.textContent, itemListArray);
+        } else {
+            alert('not');
+        }
+    });
 }
 
+
 $(document).ready(() => {
-    addKeyupEvenListenerForInput(enterKey, event);
+    addKeyupEvenListenerForInput(enterKey, keyEvent);
 
     addButton.click(() => {
         addElement(itemListArray, inputField);
@@ -76,5 +106,8 @@ $(document).ready(() => {
         clearInputField(inputField);
 
         withdrawElements(itemsList, itemListArray);
+
+        listenToItemsForClicking(checkboxClassName);
     });
+
 });
